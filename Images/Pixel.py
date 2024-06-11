8cc7ff02937d7d0b9c362d88597232ca4a310922
ItsAndrewC! <andew73885@gmail.com>
2024-06-11 15:48:20 -0400

    Added more attributes to the Pixel class

--- Images/Pixel.py
+++ Images/Pixel.py
@@ -2,11 +2,12 @@ from Images.Position import Position
 
 class Pixel:
     def __init__(self, RGBList: list, pos: Position) -> None:
-        self.c = [color for color in RGBList] # create new list
-        pass
+        self.colorList = [color for color in RGBList] # create new list
+        self.pos = pos
+        self.size = len(RGBList)*4 # size of the RGBList in Bytes
         
     def setRGB(self, cList: list) -> None:
-        self.c = [color for color in list] # overwrite previous values
+        self.c = [color for color in cList] # overwrite previous values
         
     def getRGB(self) -> tuple:
         return tuple(self.c) # Return an immutable tuple copy of the list
--- Images/Position.py
+++ Images/Position.py
@@ -1,6 +1,9 @@
 class Position:
-    def _init_(self, P: int):
-        self.pos = P
+    def _init_(self, P: list):
+        self.pos: list = [] # Row, Column #
 
     def getPos(self) -> Position: # type: ignore
         return self.pos
+    
+    def setPos(self, P:list) -> None: 
+        self.pos = P
