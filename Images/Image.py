6d164174d91cd5fdf02f2a3526c4cc653d5020d2
ItsAndrewC! <andew73885@gmail.com>
2024-06-11 15:51:20 -0400

    Removed _init_ functionality for now

--- Images/Image.py
+++ Images/Image.py
@@ -10,12 +10,6 @@ class Image:
         self.width: int = 0
         self.type: str = ""
 
-        pList: list = []
-        for byte in open(fileName, "r"):
-            pList.append(byte)
-            if i % 3 == 0:
-                self.pixelList.insert(Pixel(pList, Position(i//3)))
-            i+=1
         # Every image has a length and width, need to do some research on those
 
     def getPixelList(self) -> list:
