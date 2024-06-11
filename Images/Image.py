69bd5d0e6f8e476621f5ffb89064a53dd561c7d4
ItsAndrewC! <andew73885@gmail.com>
2024-06-10 22:03:57 -0400

    Finishing up framework for Image.py

--- Images/Image.py
+++ Images/Image.py
@@ -1,14 +1,11 @@
 from Images.Pixel import Pixel
 from Images.Position import Position
 class Image:
-    def __init__(self, fileName: str) -> None:
-        self.bitStream = [byte for byte in open(str, "r")] # Get a contiguous byteArray
-        self.pixelList = []
-
-        i = 0
+    def __init__(self, fileName: str) -> None: # Load in bytearray representing image
+        self.pixelList = []; i = 0
         pList: list = []
-        for byte in self.bitStream:
+        for byte in open(fileName, "r"):
             pList.append(byte)
-            if i % 3 != 0:
+            if i % 3 == 0:
                 self.pixelList.insert(Pixel(pList, Position(i)))
             i+=1
