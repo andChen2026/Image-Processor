10ec5e3837710ab8b8ae3ca4616aa7d6758ab26e
ItsAndrewC! <andew73885@gmail.com>
2024-06-11 15:14:28 -0400

    Added a metadate portion for Image class

--- Images/Image.py
+++ Images/Image.py
@@ -4,12 +4,19 @@ from Images.Position import Position
 class Image:
     def __init__(self, fileName: str) -> None: # Load in bytearray representing image
         self.pixelList = []; i = 0
+
+        # Metadata for Image
+        self.length: int = 0
+        self.width: int = 0
+        self.type: str = ""
+
         pList: list = []
         for byte in open(fileName, "r"):
             pList.append(byte)
             if i % 3 == 0:
                 self.pixelList.insert(Pixel(pList, Position(i//3)))
             i+=1
+        # Every image has a length and width, need to do some research on those
 
     def getPixelList(self) -> list:
         return self.pixelList
